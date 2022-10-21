69a275982acaf41279e015961a0a814ce50b1907
Victor Dan <victorandreidan@gmail.com>
2022-10-21 13:15:32 +0300

    finished final get for token with all its articles and categories

--- Day2_Tema2/addingCategories.js
+++ Day2_Tema2/addingCategories.js
@@ -1,30 +1,26 @@
 const Categories	= require('./Categories.js');
-const mongoose		= require('mongoose');
 
 async function addCategories(categoryName, articleID){
-	await mongoose.connect("mongodb://localhost/aquasofttema2");
 	const searchResult	= await Categories.find({Category_name: categoryName});
-	console.log("THE length is: ", searchResult.length);
+	console.log("CATEG THE length is: ", searchResult.length);
 	if(searchResult.length > 0 && searchResult[0].Articles.includes(articleID)) {
 		console.log("Category with name:", categoryName, "already exists and the article is already added as foreign key!");
-		return false;
+		return searchResult[0]._id;
 	} // Token already exists
 	if(searchResult.length > 0 && !searchResult[0].Articles.includes(articleID)) {
 		searchResult[0].Articles.push(articleID);
 		searchResult[0].save();
 		console.log("Category with name:", categoryName, "has added article as foreign key with id:", articleID);
-		return false;
+		return searchResult[0]._id;
 	}
 	const newCategory = new Categories({
 		Category_name: categoryName,
 		Articles: []
 	});
 	newCategory.Articles.push(articleID);
-	newCategory.save().then(()=>{
-		console.log("Added Category: ", newCategory);
-		return true;
-	});
-	await mongoose.disconnect();
+	await newCategory.save();
+	console.log("Added Category: ", newCategory);
+	return newCategory._id;
 }
 
 module.exports = addCategories;
--- Day2_Tema2/addingTokens.js
+++ Day2_Tema2/addingTokens.js
@@ -1,30 +1,26 @@
 const Tokens	= require('./Tokens.js');
-const mongoose  = require('mongoose');
 
 async function addToken(tokenBody, objectId){
-	await mongoose.connect("mongodb://localhost/aquasofttema2");
 	const searchResult = await Tokens.find({Token_body: tokenBody});
 	console.log("THE length is: ", searchResult.length);
 	if(searchResult.length > 0 && searchResult[0].Articles.includes(objectId)) {
-		console.log("Token with body:", tokenBody, "already exists and has post as primary key!");
-		return false;
+		console.log("Token with body:", tokenBody, "already exists and has post as foreign key!");
+		return searchResult[0]._id;
 	} // Token already exists
 	if(searchResult.length > 0 && !(searchResult[0].Articles.includes(objectId))){
 		searchResult[0].Articles.push(objectId);
 		searchResult[0].save();
 		console.log("Token with body:", tokenBody, "already exists but article was added as foreign key with id:", objectId);
-		return false;
+		return searchResult[0]._id;
 	}
 	const newToken = new Tokens({
 		Token_body: tokenBody,
 		Articles: []
 	});
 	newToken.Articles.push(objectId);
-	newToken.save().then(()=>{
-		console.log("Added Token: ", newToken);
-		return true;
-	});
-	await mongoose.disconnect();
+	await newToken.save();
+	console.log("Added Token: ", newToken);
+	return newToken._id;
 }
 
 module.exports = addToken;
